319dd730cfeb7eb1b35ab5c9f39a37322e404ebd
Aldo-Portillo <dapa080909hmcmrla9@soycecytem.mx>
2026-01-13 16:36:36 -0600

    Proyecto Django empleado

---
+++ applications/departamento/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Departamento(models.Model):
+    nombre = models.CharField('Nombre', max_length=50)
+    subnombre = models.CharField('Nombre corto', max_length=50, unique=True)
+    activo = models.BooleanField('Anulado', default=False)
+
+    class Meta:
+        verbose_name = 'Mi departamento'
+        verbose_name_plural = 'Areas de la empresa'
+        ordering = ['nombre']
+        unique_together = ('nombre', 'subnombre')
+
+    def __str__(self):
+        return f"{self.id} - {self.nombre} - {self.subnombre}"
